a278682536fdb530eb3cb9f8be8f06e8826d61ee
Snezhana Stateva <114188863+Stateva91@users.noreply.github.com>
2023-06-21 10:31:55 +0200

    add new tasks

---
+++ Programing JS Front-End/FunctionsAndStatements_02/1.SmallestOfThreeNumbers.js
@@ -0,0 +1,7 @@
+function SmallestOfThreeNumbers(num1, num2, num3){
+    let minNumber = Math.min(num1,num2, num3);
+
+    return minNumber;
+}
+
+const smallestOfThreeNumbers = (firstNum, secondNum, thirdNum) => Math.min(firstNum, secondNum, thirdNum);
---
+++ Programing JS Front-End/FunctionsAndStatements_02/7.NxNMatrix.js
@@ -0,0 +1,3 @@
+function NxNMatrix (){
+    let NxNMatrix = (n) => new Array(n).fill(new Array(n).fill(n)).forEach(row =>console.log(row.join(" ")));
+}
