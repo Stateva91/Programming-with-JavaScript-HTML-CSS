6f30456ce450f4f6fbfdaf78182b99ac4cfbf1b0
Snezhana Stateva <114188863+Stateva91@users.noreply.github.com>
2023-11-04 19:53:29 +0100

    4.Print and Sum

--- Programing JS Front-End/Exercises JS Syntax Fundamentals/03. LeapYear.js
+++ Programing JS Front-End/Exercises JS Syntax Fundamentals/03. LeapYear.js
@@ -1,5 +1,15 @@
 function leapYear(year){
-    if((year%4 ===0 && year%100 !=0) || year%400===0){
+    // if((year%4 ===0 && year%100 !=0) || year%400===0){
+    //     console.log("yes");
+    // }else {
+    //     console.log("no");
+    // }
+
+    let date=new Date(year, 1, 1);
+    date.setMonth(date.getMonth()+1);
+    date.setDate(date.getDate()-1);
+    console.log(date);
+    if(date.getDate()===29){
         console.log("yes");
     }else {
         console.log("no");
