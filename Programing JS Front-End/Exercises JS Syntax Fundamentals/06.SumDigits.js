ddcf4963ec4461a0e89d32507ba3d1cf20ea7837
Snezhana Stateva <114188863+Stateva91@users.noreply.github.com>
2023-11-04 21:19:40 +0100

    6.Sum Digits

---
+++ Programing JS Front-End/Exercises JS Syntax Fundamentals/06.SumDigits.js
@@ -0,0 +1,13 @@
+function sumDigits(num){
+
+    let numAsString=String(num);
+    let sum=0;
+
+    for(let i=0; i<numAsString.length; i++){
+       let currentNum=Number(numAsString[i]);
+       sum+=currentNum
+    }
+   console.log(sum);
+}
+
+sumDigits(245678);
