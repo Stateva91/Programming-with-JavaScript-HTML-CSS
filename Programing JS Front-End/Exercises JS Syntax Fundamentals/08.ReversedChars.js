4790b332b4d98d1835150afa50c16097d75cf895
Snezhana Stateva <114188863+Stateva91@users.noreply.github.com>
2023-11-05 13:39:12 +0100

    08. Reversed Char

--- Programing JS Front-End/Exercises JS Syntax Fundamentals/08.ReversedChars.js
+++ Programing JS Front-End/Exercises JS Syntax Fundamentals/08.ReversedChars.js
@@ -1,4 +1,10 @@
-function reversedChars(){
+function reversedChars(char1, char2, char3){
 
+    let result = [];
+    result.push(char1);
+    result.push(char2);
+    result.push(char3)
+
+    console.log(result.reverse().join(" "));
     
 }
