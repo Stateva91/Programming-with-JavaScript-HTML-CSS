06976d7ce91f6dfd6f9f2de18e8b1967f717f9de
Snezhana Stateva <114188863+Stateva91@users.noreply.github.com>
2023-11-16 20:15:33 +0100

    05. Palindrome Integers

---
+++ Programing JS Front-End/Exercises Functions And Statements/05. PalindromeIntegers.js
@@ -0,0 +1,18 @@
+function palindrome(arr) {
+
+    
+
+    function isPalindrome(num){
+
+        let numAsString=num.toString();
+        let reversedNum=numAsString.split('').reverse().join('');
+
+        return numAsString===reversedNum;
+    }
+    
+    for (const currentNumber of arr) {
+        console.log(isPalindrome(currentNumber));
+        
+    }
+    
+}
