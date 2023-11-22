ef8208ff45164c4e11d4a23c1a6091a85e6e065a
Snezhana Stateva <114188863+Stateva91@users.noreply.github.com>
2023-11-22 14:27:39 +0100

    Inventory

---
+++ Programing JS Front-End/Exercise Objects and Classes/5. Inventory.js
@@ -0,0 +1,31 @@
+function inventory(input) {
+
+    let heroes=[];
+   for (const heroData of input) {
+   
+    let [name, level, items]= heroData.split(' / ');
+    let hero={
+        name: name,
+        level: Number(level),
+        items: items
+    }
+    heroes.push(hero);
+   }
+
+   heroes.sort((a,b)=>a.level - b.level);
+   //heroes.foreach(h=>console.log(h));
+
+   for (const hero of heroes) {
+    
+    console.log(`Hero: ${hero.name}`);
+    console.log(`level => ${hero.level}`);
+    console.log(`items => ${hero.items}`);
+   }
+    
+}
+
+inventory([
+    'Isacc / 25 / Apple, GravityGun',
+    'Derek / 12 / BarrelVest, DestructionSword',
+    'Hes / 1 / Desolator, Sentinel, Antara'
+    ])
