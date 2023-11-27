f22a451228879a3cf5a6212d14da19656fc0c8f8
Snezhana Stateva <114188863+Stateva91@users.noreply.github.com>
2023-11-27 21:57:18 +0100

    Generate Report

--- Programing JS Front-End/Exercise DOM Introduction/08. Generate Report/report.js
+++ Programing JS Front-End/Exercise DOM Introduction/08. Generate Report/report.js
@@ -1,3 +1,33 @@
 function generateReport() {
-    //TODO
+    
+    let personObject={};
+    let personalAllObject=[];
+    let columnSetNumbers=[];
+
+    let columnsAll=document.querySelectorAll('thead tr th input');
+
+    for (let index = 0; index < columnsAll.length; index++) {
+       let checkBoxState=columnsAll[index].checked;
+       
+       if(checkBoxState){
+        columnSetNumbers.push(index);
+       }
+        
+    }
+    let columnTitles=document.querySelectorAll('thead tr')[0].getElementsByTagName('th');
+
+    let rowsCount=document.querySelectorAll('tbody tr').length;
+
+    for (let row = 0; row < rowsCount; row++) {
+        columnSetNumbers.forEach((element)=>{
+            let key=columnTitles[element].textContent.trim().toLowerCase();
+            let value=document.querySelectorAll('tbody tr')[row].getElementsByTagName('td')[element].textContent;
+            personObject[key]=value;
+        });
+
+        personalAllObject.push(Object.assign(personObject));
+        personObject={};
+        
+    }
+    document.getElementById('output').innerHTML=JSON.stringify(personalAllObject);
 }
