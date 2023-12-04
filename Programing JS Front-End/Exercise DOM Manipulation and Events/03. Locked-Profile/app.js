541d1af17dd64e8a4187c8e56a41ba3ce1156274
Snezhana Stateva <114188863+Stateva91@users.noreply.github.com>
2023-12-04 15:19:58 +0100

    Locked Profile

--- Programing JS Front-End/Exercise DOM Manipulation and Events/03. Locked-Profile/app.js
+++ Programing JS Front-End/Exercise DOM Manipulation and Events/03. Locked-Profile/app.js
@@ -1,3 +1,30 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let buttons=Array.from(document.getElementsByTagName('button'));
+
+    buttons.forEach((button)=>{
+        button.addEventListener('click', showMoreInfo);
+    });
+
+    function showMoreInfo(event){
+
+        let btn=event.target;
+        let profile=btn.parentElement;
+        let children=Array.from(profile.children);
+
+        let unLocked=children[4];
+        let additionalInfo=children[9];
+
+        if(!unLocked.checked){
+            return;
+        }
+
+        if(btn.textContent === 'Show more'){
+            additionalInfo.style.display='block';
+            btn.textContent='Hide it'
+        }else if ( btn.textContent='Hide it'){
+            additionalInfo.style.display ='none';
+            btn.textContent='Show more'
+        }
+
+    }
 }
