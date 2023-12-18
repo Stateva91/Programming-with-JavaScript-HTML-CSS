9b3e1093c0bac07f7dd2a864452f277cc602a1ee
Snezhana Stateva <114188863+Stateva91@users.noreply.github.com>
2023-12-18 20:48:14 +0100

    Exam

--- Programing JS Front-End/JS Front-end Exam 17 August 2023/02. Dart Scoring/app.js
+++ Programing JS Front-End/JS Front-end Exam 17 August 2023/02. Dart Scoring/app.js
@@ -1,6 +1,7 @@
 window.addEventListener("load", solve);
 
 function solve() {
+
     let playerNameInput=document.getElementById('player');
     let playerScore=document.getElementById('score');
     let round=document.getElementById('round');
@@ -32,11 +33,17 @@ function solve() {
       let nameParagraph=document.createElement('p');
       nameParagraph.textContent=playerNameInput.value;
 
+      let nameVal=playerNameInput.value;
+
       let scoreParagraph=document.createElement('p');
       scoreParagraph.textContent=`Score: ${playerScore.value}`;
 
+      let scoreVal=playerScore.value;
+
       let roundParagraph=document.createElement('p');
       roundParagraph.textContent=`Round: ${round.value}`;
+
+      let roundVal=round.value;
 
       articleElement.appendChild(nameParagraph);
       articleElement.appendChild(scoreParagraph);
@@ -50,7 +57,7 @@ function solve() {
 
       let postBtn=document.createElement('button');
       postBtn.classList.add('btn');
-      editBtn.classList.add('ok');
+      postBtn.classList.add('ok');
       postBtn.textContent='ok';
       postBtn.addEventListener('click',post);
 
@@ -62,6 +69,30 @@ function solve() {
 
       publishBtn.disabled=true;
       formElement.reset();
-    }
+
+      function edit(){
+        playerNameInput.value=nameVal;
+        playerScore.value=scoreVal;
+        round.value=roundVal;
+
+        reviewList.removeChild(li);
+        publishBtn.disabled=false;
+
+        
+      }
+      function post(){
+        reviewList.removeChild(li);
+        li.removeChild(postBtn);
+        li.removeChild(editBtn);
+        publishedList.appendChild(li);
+
+        publishBtn.disabled=false;
+
+        clearBtn.addEventListener('click', ()=>{
+          location.reload();
+        });
+
+      }
+        }
   }
   
