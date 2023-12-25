f2e52d3a3a8d4625339d7ee99e7a67e881638d2a
Snezhana Stateva <114188863+Stateva91@users.noreply.github.com>
2023-12-25 11:58:09 +0100

    task 3

--- Programing JS Front-End/Exam2/03.Christmas-Gifts_Resources/app.js
+++ Programing JS Front-End/Exam2/03.Christmas-Gifts_Resources/app.js
@@ -1,150 +1,201 @@
-const baseUrl = 'http://localhost:3030/jsonstore/gifts';
+const BASE_URL = 'http://localhost:3030/jsonstore/gifts';
 
-const loadGiftsButton = document.getElementById('load-presents');
-const giftsList = document.getElementById('gift-list');
-const formElement=document.querySelector('#form form');
-const giftInput=document.getElementById('gift');
-const forInput=document.getElementById('for');
-const priceInput=document.getElementById('price');
-const formAddButton=document.getElementById('add-present');
-const formEditButton=document.getElementById('edit-present');
+const endpoints = {
+    update: (id) => `${BASE_URL}/${id}`,
+    delete: (id) => `${BASE_URL}/${id}`,
+};
 
+const giftElement = document.getElementById("gift");
+const priceElement = document.getElementById("price");
+const forElement = document.getElementById("for");
 
-loadGiftsButton.addEventListener('click', loadGift);
+const presentsList = document.getElementById("presents");
+const list = document.getElementById('gift-list');
 
-formAddButton.addEventListener('click', (e)=>{
-    e.preventDefault();
+const addBtn = document.getElementById("add-present");
+const editBtn = document.getElementById("edit-present");
+const loadBtn = document.getElementById("load-presents");
+const clearBtn = document.querySelector(".clear-btn");
 
-    //get data from inputs
-    const newGift={
-        gift: giftInput.value,
-        forName: forInput.value,
-        price: priceInput.value,
-    };
-    //send post request to server
-     fetch(baseUrl, {
-        method: 'POST',
-        headers:{
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(newGift)
-     })
-     .then(loadGift)
-     
+let selectedTaskId = null;
+
+function attachEvents() {
+    loadBtn.addEventListener('click', loadBoardEventHandler);
+    addBtn.addEventListener('click', createTaskEventHandler);
+    editBtn.addEventListener('click', editTaskEventHandler);
+}
+
+function getIdByGift(gift) {
+    return fetch(BASE_URL)
+        .then(res => res.json())
+        .then(res => Object.entries(res).find(e => e[1].gift == gift)[1]._id);
+}
+
+async function loadBoardEventHandler() {
+    clearAllSections();
+    try {
+        const res = await fetch(BASE_URL);
+        const allGifts = await res.json();
+        Object.values(allGifts).forEach((gift) => {
+            const container = document.createElement('div');
+            container.className = 'gift-sock';
+
+            const content = document.createElement('div');
+            content.className = 'content';
+
+            const figtElement = document.createElement('p');
+            figtElement.textContent = gift.gift;
+
+            const priceElement = document.createElement('p');
+            priceElement.textContent = gift.price;
+
+            const forElement = document.createElement('p');
+            forElement.textContent = gift.for;
+           
+
+            const buttonsContainer = document.createElement('div'); 
+            buttonsContainer.className = 'buttons-container';
+
+            const changeBtn = document.createElement('button');
+            changeBtn.className = 'change-btn';
+            changeBtn.textContent = 'Change';
+
+            const doneBtn = document.createElement('button');
+            doneBtn.className = 'delete-btn';
+            doneBtn.textContent = 'Delete';
+
+            buttonsContainer.appendChild(changeBtn); 
+            buttonsContainer.appendChild(doneBtn);
+
+            content.appendChild(figtElement);
+            content.appendChild(priceElement);
+            content.appendChild(forElement);
+
+            container.appendChild(content);
+            container.appendChild(buttonsContainer); 
+
+            list.appendChild(container);
+        });
+        attachEventListeners();
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+function attachEventListeners() {
+    const changeButtons = document.querySelectorAll('.change-btn');
+    const doneButtons = document.querySelectorAll('.delete-btn');
+
+    changeButtons.forEach((changeButton) => {
+        changeButton.addEventListener('click', (event) => {
+            const taskElement = event.target.closest('.gift-sock');
+            const gift = taskElement.querySelector('p').textContent;
+            const price = taskElement.querySelector('p:nth-child(2)').textContent;
+            const forE = taskElement.querySelector('p:nth-child(3)').textContent;
+            editTask(gift, price, forE);
+            enableEditBtn();
+        });
+    });
     
-    clearForm();
+
+    doneButtons.forEach((doneButton) => {
+        doneButton.addEventListener('click', (event) => {
+            const taskElement = event.target.closest('.gift-sock');
+            const gift = taskElement.querySelector('p').textContent;
+            deleteTask(gift);
+        });
+    });
     
+}
 
-})
-formEditButton.addEventListener('click', (e)=>{
-    e.preventDefault();
+function enableEditBtn() {
+    addBtn.disabled = true;
+    editBtn.disabled = false;
+}
 
-    const giftId=formElement.dataset.newGift;
+function enableAddBtn() {
+    addBtn.disabled = false;
+    editBtn.disabled = true;
+}
 
-    // get data
-    const giftData={
-        _id: formElement.dataset.newGift,
-        gift: giftInput.value,
-        forName: forInput.value,
-        price: priceInput.value,
+function createTaskEventHandler(ev) {
+    ev.preventDefault();
+    if (giftElement.value !== '' && forElement.value !== '' && priceElement.value !== '') {
+        const headers = {
+            method: 'POST',
+            body: JSON.stringify({
+                gift: giftElement.value,
+                for: forElement.value,
+                price: priceElement.value,
+            }),
+        };
+
+        fetch(BASE_URL, headers)
+            .then(loadBoardEventHandler)
+            .catch(console.error);
+
+        clearAllInputs();
+    }
+}
+
+async function editTask(gift, priceGift, forGift) {
+    selectedTaskId = await getIdByGift(gift);
+    giftElement.value = gift;
+    priceElement.value = priceGift;
+    forElement.value = forGift;
+}
+
+function editTaskEventHandler(ev) {
+    ev.preventDefault();
+    const gift = giftElement.value;
+    const data = {
+        gift: giftElement.value,
+        for: forElement.value,
+        price: priceElement.value,
+        _id: selectedTaskId,
     };
-    //put request
-    fetch(`${baseUrl}/${giftId}`,{
+
+    fetch(endpoints.update(data._id), {
         method: 'PUT',
         headers: {
-            'Content-type': 'application/json'
+            'Content-Type': 'application/json',
         },
-        body: JSON.stringify(giftData)
+        body: JSON.stringify(data),
     })
-    .then(loadGift)
-   
-    .then(()=>{
-       formAddButton.removeAttribute('disabled');
-       formEditButton.setAttribute('disabled', 'disabled');
-    
-       clearForm();
-
-       delete formElement.dataset.newGift;
-    
-    })
-  
-
-
-})
-function clearForm (){
-    giftInput.value='';
-    forInput.value='';
-    priceInput.value='';
-}
-function loadGift(){
-    return fetch(baseUrl)
-    .then(res => res.json())
-    .then(result => {
-        renderGifts(Object.values(result));
-    });
+        .then(() => {
+            clearAllInputs();
+            loadBoardEventHandler();
+            selectedTaskId = null;
+            enableAddBtn();
+        })
+        .catch(console.error);
 }
 
-function renderGifts(gifts){
-    giftsList.innerHTML='';
-    gifts
-    .map(renderGift)
-    .forEach(giftElement=>giftsList.appendChild(giftElement));
-
+function deleteTask(taskLoacation) {
+    getIdByGift(taskLoacation)
+        .then((id) =>
+            fetch(endpoints.delete(id), {
+                method: 'DELETE',
+                headers: { 'Content-Type': 'application/json' },
+            })
+        )
+        .then(() => {
+            clearAllSections();
+            loadBoardEventHandler();
+            selectedTaskId = null;
+            enableAddBtn();
+        })
+        .catch(console.error);
 }
 
-function renderGift(newGift){
-
-
-    const container=document.createElement('div');
-    container.className='content';
-
-    const giftElement=document.createElement('p');
-    giftElement.textContent=newGift.gift;
-
-    const nameElement=document.createElement('p');
-    nameElement.textContent=newGift.price;
-
-    const priceElement=document.createElement('p');
-    priceElement.textContent=newGift.forName;
-
-    const changeButton=document.createElement('button');
-    changeButton.className='change-btn';
-    changeButton.textContent='Change';
-    changeButton.addEventListener('click', ()=>{
-
-        //add to form  fields
-    giftInput.value=newGift.gift;
-    forInput.value=newGift.forName;
-    priceInput.value=newGift.price;
-      //remove from confirmed list
-      container.remove();
-        //Activate edit newGift button
-        formEditButton.removeAttribute('disabled');
-
-        //deactivate add newGift button
-       formAddButton.setAttribute('disabled', 'disabled');
-
-       //save newGift id
-       formElement.dataset.newGift=newGift._id;
-    })
-
-    const deleteButton=document.createElement('button');
-    deleteButton.className='delete-btn';
-    deleteButton.textContent='Delete';
-    deleteButton.addEventListener('click', ()=>{
-       //send delete request
-       fetch(`${baseUrl}/${newGift._id}`,{
-       method: 'DELETE',
-    })
-    .then(loadGift);
-       //load newGift
-    })
-
-    container.appendChild(giftElement);
-    container.appendChild(nameElement);
-    container.appendChild(priceElement);
-    container.appendChild(changeButton);
-    container.appendChild(deleteButton);
-
-    return container;
+function clearAllSections() {
+    list.innerHTML = '';
 }
+
+function clearAllInputs() {
+    giftElement.value = '';
+    forElement.value = '';
+    priceElement.value = '';
+}
+
+attachEvents();
